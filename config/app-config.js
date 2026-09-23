bbfde76c17b4154a252049cb08fa85f70f3e26b7
sschoudharyshubhammundhar-stack <sschoudharyshubhammundhar@gmail.com>
2026-09-23 17:29:04 +0530

    config: repair public app configuration formatting

--- config/app-config.js
+++ config/app-config.js
@@ -4,7 +4,9 @@
 window.SHUBHMART_CONFIG = Object.freeze({
   appName: "ShubhMart",
   tagline: "Har Zaroorat, Ek Jagah",
-  currency: "INR",\n  supabaseUrl: "https://yauveaexbpokhytqijae.supabase.co",\n  supabasePublishableKey: "sb_publishable_79SNGwvCzpL4OqWBAInVoA_d4xFht6g",
+  currency: "INR",
+  supabaseUrl: "https://yauveaexbpokhytqijae.supabase.co",
+  supabasePublishableKey: "sb_publishable_79SNGwvCzpL4OqWBAInVoA_d4xFht6g",
   mode: "demo",
   features: Object.freeze({
     retailMarketplace: true,
